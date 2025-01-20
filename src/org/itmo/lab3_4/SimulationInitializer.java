a86f9b4b38bc6369ddb3a5a1dd65152a8ea9b40a
andrey20005 <z.andrey2005.10@gmail.com>
2025-01-21 01:51:03 +0300

    Код на переделку

---
+++ src/org/itmo/lab3_4/SimulationInitializer.java
@@ -0,0 +1,43 @@
+package org.itmo.lab3_4;
+
+import org.itmo.lab3_4.Human;
+import org.itmo.lab3_4.features.*;
+
+public class SimulationInitializer {
+    public static void main(String[] args) throws ExceptionOfHavingOnePrioritie {
+        Simulation simulation = new Simulation();
+
+        Human pilulkin = new Human("Пилюлькин");
+        pilulkin.addFeature(new Hardworking(pilulkin));
+        pilulkin.addFeature(new WorksAsDoctor(pilulkin));
+        simulation.addHuman(pilulkin);
+
+        Human vintik = new Human("Винтик");
+        vintik.addFeature(new Hardworking(vintik));
+        vintik.addFeature(new WorksAsMechanic(vintik));
+        simulation.addHuman(vintik);
+
+        Human siropchik = new Human("Сиропчик");
+        siropchik.addFeature(new NormalPriorities(siropchik));
+        siropchik.addFeature(new LoveSoda(siropchik));
+        simulation.addHuman(siropchik);
+
+        Human toptyshka = new Human("Торопыжка");
+        toptyshka.addFeature(new NormalPriorities(toptyshka));
+        simulation.addHuman(toptyshka);
+
+        Human malyshka = new Human("Малышка");
+        malyshka.addFeature(new NormalPriorities(malyshka));
+        simulation.addHuman(malyshka);
+
+        Human neznaika = new Human("Незнайка");
+        neznaika.addFeature(new Lazy(neznaika));
+        neznaika.addFeature(new TellTallTales(neznaika));
+        neznaika.addFeature(new LikesToOffend(neznaika));
+        simulation.addHuman(neznaika);
+
+        simulation.goSimulationStep();
+        simulation.goSimulationStep();
+    }
+}
+
